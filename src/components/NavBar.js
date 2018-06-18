ca27f06a8a582a98dba343a6d49d58c547fe5eb9
Dan Roberts <dan@adadevelopersacademy.org>
2018-06-18 11:01:28 -0700

    dpr/ddr - there's a nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import './NavBar.css';
 
 class NavBar extends Component {
   render() {
     return (
-      <div>NavBar</div>
+      <div className='navbar'>
+        <nav className='navbar__nav'>
+          <Link to="/search" className='navbar__nav-item'>Search TMDB</Link>
+          <Link to="/library" className='navbar__nav-item'>List Rental Library</Link>
+          <Link to="/customers" className='navbar__nav-item'>List Customers</Link>
+        </nav>
+
+      </div>
     );
   }
 }
