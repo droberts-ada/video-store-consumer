1f4bfd207723ef8e1d90117e4aaff652867f6cf0
Dan Roberts <dan@adadevelopersacademy.org>
2018-06-18 13:35:07 -0700

    Actually add the status bar

---
+++ src/components/StatusBar.js
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import './StatusBar.css';
+
+class StatusBar extends React.Component {
+  static propTypes = {
+    message: PropTypes.string.isRequired,
+    type: PropTypes.oneOf(['error', 'success']),
+    clearStatus: PropTypes.func.isRequired,
+  }
+
+
+
+  render() {
+    let className = "status-bar "
+    if (this.props.message.length === 0) {
+      className += "status-bar--hide"
+    } else if (this.props.type === 'error') {
+      className += "status-bar--error"
+    } else {
+      className += "status-bar--success"
+    }
+    return(
+      <div className={className}>
+        <p className="status-bar__text">
+          {this.props.message}
+        </p>
+        <button
+          className="status-bar__button"
+          onClick={this.props.clearStatus}
+          >
+          X
+        </button>
+      </div>
+    );
+  }
+}
+
+export default StatusBar;
