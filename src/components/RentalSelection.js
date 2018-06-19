714a4133d6ee3862b02d3672665a33374b2f2a1a
Dan Roberts <dan@adadevelopersacademy.org>
2018-06-18 18:11:19 -0700

    add forgotten components

---
+++ src/components/RentalSelection.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './RentalSelection.css';
+
+const RentalSelection = ({selectionType, name}) => {
+  return (
+    <div className="rental-selection header__item">
+      <label className="rental-selection__label">
+        Selected {selectionType}
+      </label>
+      <p className="rental-selection__name">
+        {name}
+      </p>
+    </div>
+  )
+}
+
+export default RentalSelection;
